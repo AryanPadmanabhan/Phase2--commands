bbbf604c59f36f8cb696ea914734fcf4b6ddd61e
Aryan <67077646+AryanPadmanabhan@users.noreply.github.com>
2022-10-19 18:09:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from discord import app_commands
 import yfinance as yf
 import pandas as pd
 from prettytable import PrettyTable
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import time
 
 class aclient(discord.Client):
     def __init__(self):
@@ -12,7 +16,7 @@ class aclient(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced: #check if slash commands have been synced 
-            await tree.sync(guild = discord.Object(id=[INSERT ID HERE])) #guild specific: leave blank if global (global registration can take 1-24 hours)
+            await tree.sync(guild = discord.Object(id=ID)) #guild specific: leave blank if global (global registration can take 1-24 hours)
             self.synced = True
         print(f"We have logged in as {self.user}.")
 
@@ -27,7 +31,41 @@ def rvol(stock):
 client = aclient()
 tree = app_commands.CommandTree(client)
 
+def get_sq():
+  chrome_options = webdriver.ChromeOptions()
+  chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.add_argument("--no-sandbox")
+  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+  driver.get('https://squeezemetrics.com/monitor/dix')
+  screenshot = driver.save_screenshot('14451982418253.png')
+  driver.quit()
 
+def get_swaggy():
+  chrome_options = webdriver.ChromeOptions()
+  chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.add_argument("--no-sandbox")
+  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+  driver.get('https://swaggystocks.com/dashboard/wallstreetbets/ticker-sentiment')
+  driver.execute_script("window.scrollBy(0,800)","")
+  time.sleep(1)
+  screenshot = driver.save_screenshot('swaggy.png')
+  driver.quit()
+
+def get_twitter():
+  chrome_options = webdriver.ChromeOptions()
+  chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.add_argument("--no-sandbox")
+  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+  driver.get('https://tradestie.com/apps/twitter/most-active-stocks/')
+  time.sleep(1)
+  screenshot = driver.save_screenshot('twitter.png')
+  driver.quit()
 
 def topvol(stock, choice):
   c = -2
@@ -67,25 +105,61 @@ def topvol(stock, choice):
       myTable.add_row([str(expiration), str(best_options[expiration][0]) + 'p', best_options[expiration][1]])
     return myTable
 
+def get_short_info(stock):
+
+  short_float = yf.Ticker(stock).info['shortPercentOfFloat']
+  short_float = short_float * 100
+
+  return short_float
 
 
-
-@tree.command(guild = discord.Object(id=[INSERT ID HERE]), name = 'rvol', description='Get the Relative Volume of any stock') #guild specific slash command
-async def self(interaction: discord.Interaction, stock: str, con: str):
+@tree.command(guild = discord.Object(id=ID), name = 'rvol', description='Get the Relative Volume of any stock') #guild specific slash command
+async def self(interaction: discord.Interaction, stock: str):
+  await interaction.response.defer()
   rvolume = rvol(stock)
   embed = discord.Embed(
-    title=f'rVol of {stock}',
+    title=f'Relative Vol. of {stock}',
     description=f'```diff\n- Relative Volume {round(rvolume, 2)}```',
     color=discord.Colour.green())
-  await interaction.response.send_message(embed=embed)
+  await interaction.followup.send(embed=embed)
 
-@tree.command(guild = discord.Object(id=[INSERT ID HERE]), name = 'topvol', description='Get the most liquid options of any stock') #guild specific slash command
-async def self(interaction: discord.Interaction, stock: str, contract: str):
-  options = topvol(stock, contract)
+@tree.command(guild = discord.Object(id=ID), name = 'topvol', description='Get the most liquid options of any stock') #guild specific slash command
+async def self(interaction: discord.Interaction, stock: str, c_or_p: str):
+  await interaction.response.defer()
+  options = topvol(stock, c_or_p)
   embed = discord.Embed(
     title=f'Liquid Options',
     description=f'```diff\n+ Most Liquid Options for {stock} \n{options}```',
     color=discord.Colour.green())
-  await interaction.channel.send(embed=embed) 
+  await interaction.followup.send(embed=embed)
 
-client.run(INSERT AUTH CODE)
+
+@tree.command(guild = discord.Object(id=ID), name = 'sfloat', description='Get the short float of any stock') #guild specific slash command
+async def self(interaction: discord.Interaction, stock: str):
+  await interaction.response.defer()
+  short_float = get_short_info(stock)
+  embed = discord.Embed(
+    title=f'Short Float',
+    description=f'```diff\n+ Short float for {stock}: {round(short_float, 2)}%```',
+    color=discord.Colour.green())
+  await interaction.followup.send(embed=embed) 
+
+@tree.command(guild = discord.Object(id=ID), name = 'sqme', description='Get the squeeze metrics') #guild specific slash command 
+async def self(interaction: discord.Interaction):
+  await interaction.response.defer()
+  get_sq()
+  await interaction.followup.send(file = discord.File('14451982418253.png')) 
+
+@tree.command(guild = discord.Object(id=ID), name = 'wsb', description='Get trending stocks on WSB') #guild specific slash command 
+async def self(interaction: discord.Interaction):
+  await interaction.response.defer()
+  get_swaggy()
+  await interaction.followup.send(file = discord.File('swaggy.png')) 
+
+@tree.command(guild = discord.Object(id=ID), name = 'twitter', description='Get trending stocks on Twitter') #guild specific slash command 
+async def self(interaction: discord.Interaction):
+  await interaction.response.defer()
+  get_twitter()
+  await interaction.followup.send(file = discord.File('twitter.png')) 
+
+client.run('TOKEN')
